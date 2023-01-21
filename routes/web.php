9bfe413e08d340cd3fa82443d56080dd78781369
Matheus MR <mmussattorodrigues@gmail.com>
2023-01-21 14:53:20 -0300

    ROUTES

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,9 @@ Route::get('/sobre', function () {
 });
 
 Route::resource('vaga', FormController::class);
-Route::resource('auth', CustomAuthController::class);
 
-Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
 Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
 Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
